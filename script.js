a0ec80c728ef2fea3d21510531d74e33ec0e2f49
Alex <alexandra_hogue@brown.edu>
2024-04-28 21:27:50 -0400

    feat: pages added

---
+++ script.js
@@ -0,0 +1,39 @@
+const star = document.getElementById("star");
+let newX = 0, newY = 0, startX = 100, startY = 100;
+let offsetX = 0,
+  offsetY = 0;
+let isDragging = false;
+
+star.addEventListener('mousedown', mouseDown)
+
+function mouseDown(e){
+  isDragging = true;
+  e.preventDefault();
+  startX = e.clientX
+  startY = e.clientY
+
+
+  document.addEventListener('mousemove', mouseMove)
+  document.addEventListener("mouseup", mouseUp);
+
+  offsetX = star.getBoundingClientRect().left - startX;
+  offsetY = star.getBoundingClientRect().top - startY;
+}
+
+function mouseMove(e){
+  newX = startX - e.clientX
+  newY = startY - e.clientY 
+
+  const newPosX = e.clientX + offsetX;
+  const newPosY = e.clientY + offsetY;
+
+  star.style.left = newPosX + "px";
+  star.style.top = newPosY + "px";
+
+
+}
+
+function mouseUp(e) {
+  isDragging = false;
+  document.removeEventListener("mousemove", mouseMove)
+}
